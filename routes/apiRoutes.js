9b77210ae1c76a22df644d49b3423bc6c7246af7
rehlingera <41902228+rehlingera@users.noreply.github.com>
2019-01-16 18:54:38 -0600

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ var db = require("../models");
 
 module.exports = function(app) {
   //Submit a character
-  app.post("/creations/all", function(req, res) {
+  app.post("/form/:type", function(req, res) {
     if(req.params.type = "dnd5e"){
     db.Dnd5e.create(req.body).then(function(character) {
       res.json(character);
