30e4718fd3cef6d55136b49df744bb2079f95f57
Jacob Franklin <jacob.franklin@healthtel.net>
2019-01-16 21:35:27 -0600

    workingJsPost

--- public/js/hero-genesis.js
+++ public/js/hero-genesis.js
@@ -5,8 +5,34 @@ $(document).ready(function(){
       })
     });
   $("#submitType").on("click", function(){
-    $.get("/form/:type", function(data){
+    var type = "dnd5e";
+    $.get("/form/:type", type, function(data){
       $("body").html(data);
     })
+  });
+  $("#submitFormDnd5e").on("click", function(e){
+    e.preventDefault();
+    console.log("CLICKED");
+    var type= "dnd5e";
+    var newCharacter = {
+        name: $("#nameText").val(),
+        class: $("#classText").val(),
+        level: $("#levelVal").val(),
+        background: $("#backgroundText").val(),
+        race: $("#raceText").val(),
+        alignment: $("#alignmentText").val(),
+        experience: $("#experienceVal").val(),
+        personalityTraits: $("#personalityTraitsText").val(),
+        ideals: $("#idealsText").val(),
+        bonds: $("#bondsText").val(),
+        flaws: $("#flawsText").val(),
+        featuresAndTraits: $("#featuresAndTraitsText").val()
+    }
+    console.log("Loggggggging");
+    $.post("/form/" + type, newCharacter, function(data){
+      console.log("New post="+data)
+    }).then($.get("/creations/all)", function(data){
+      $("body").html(data);
+    }));
   });
 });
