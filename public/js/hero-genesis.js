e13e401e863cf85afe4e538e0c252753df849edd
Lamuel27 <samrelliner@gmail.com>
2019-01-21 22:33:40 -0600

    added images

--- public/js/hero-genesis.js
+++ public/js/hero-genesis.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
   });
 
   $(function () {
-    var images = ['../images/elf1.png', '../images/elf2.png', '../images/wizard.png', '../images/wizard2.png'];
+    var images = ['../images/elf1.png', '../images/elf2.png', '../images/wizard.png', '../images/wizard2.png','../images/orc.png','../images/viking.png'];
     $('#myPicture').css({ 'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')' });
   });
 
